3fb1ec2522ec7c1193419105a116e320a54ef20c
bpkotowski <bighoopee@gmail.com>
2018-10-03 20:45:43 -0700

    what?

---
+++ index.js
@@ -0,0 +1,9 @@
+var countT0 =( how high should we count even numbers?)
+
+
+for(var i = 0; i < 10; i++) {
+    var  y = i * 2;
+    if(y < 10) {
+        console. log('the number doubled is', y);
+    }
+}
